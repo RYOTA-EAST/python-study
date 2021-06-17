ea71aebe31d33d98c9712e62aa01ed678783ff09
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-06-17 23:12:07 +0900

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+# 変数宣言
+num1 = 1
+name = 'mike'
+is_ok = True
+
+print("こんにちは")
+
+print(num1, type(num1))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+print('Hi','Mike')
+print('Hi','Mike',sep=',',end='.\n')
