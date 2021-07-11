75ff515e6143d579a6eda5ea99561b99d315a0d4
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-07-11 23:50:55 +0900

    グラフィック作成

---
+++ graphic/Turtle.py
@@ -0,0 +1,28 @@
+import turtle
+
+for i in range(200):
+    turtle.fd(i)
+    turtle.left(360 / 4 + 10)
+
+# turtle.pencolor('green')
+
+# for i in range(60):
+#     turtle.fd(50)
+#     turtle.left(360 / 3 + 10)
+
+# turtle.pencolor('red')
+
+# for i in range(60):
+#     turtle.fd(200)
+#     turtle.left(360 / 3 + 10)
+
+
+# turtle.color('red', 'yellow')
+
+# turtle.begin_fill()
+# for i in range(5 * 3):
+#     turtle.forward(100 + i * 10)
+#     turtle.right(360 / 5 * 2)
+# turtle.end_fill()
+
+turtle.done()
