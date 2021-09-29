99f943a6fa68251fbe900540a63daa963c9bbeda
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-09-29 23:48:24 +0900

    特定ワードを含むツイート検索

---
+++ twitter_api/searchTweet.py
@@ -0,0 +1,19 @@
+import tweepy, config
+
+# 認証に必要なキーとトークン
+API_KEY = config.API_KEY
+API_SECRET = config.API_SECRET
+ACCESS_TOKEN = config.ACCESS_TOKEN
+ACCESS_TOKEN_SECRET = config.ACCESS_TOKEN_SECRET
+
+# APIの認証
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# キーワードからツイートを取得
+api = tweepy.API(auth)
+tweets = api.search(q=['"ねこプロ"'], count=10)
+
+for tweet in tweets:
+    print('-----------------')
+    print(tweet.text)
